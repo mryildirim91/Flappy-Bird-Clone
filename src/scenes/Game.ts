97734119647c50a99598216509f3d411d156fe94
Mehmet Rıdvan Yıldırım <mehmetridvanyildirim@gmail.com>
2025-02-12 11:42:31 +0300

    pipe spawn

--- src/scenes/Game.ts
+++ src/scenes/Game.ts
@@ -3,9 +3,10 @@ import TileSprite = Phaser.GameObjects.TileSprite;
 
 export default class Game extends Phaser.Scene
 {
-	private player : Phaser.Physics.Arcade.Sprite;
+	private player!: Phaser.Physics.Arcade.Sprite;
 	private jumpVelocity : number = -200;
-	//private scoreCounter : number = 0;
+	private pipeGroup!: Phaser.Physics.Arcade.Group;
+	private pipeSpawnTimer!: Phaser.Time.TimerEvent;
 
 	public constructor()
 	{
@@ -30,6 +31,18 @@ export default class Game extends Phaser.Scene
 		this.input.on("pointerdown", this.fly, this);
 		this.input.on("pointerup", this.fall, this);
 
+		this.pipeGroup = this.physics.add.group({
+			allowGravity: false
+		});
+
+		// Spawn pipes every 1.5 seconds
+		this.pipeSpawnTimer = this.time.addEvent({
+			delay: 1500, // Spawn every 1.5 seconds
+			callback: this.spawnPipe,
+			callbackScope: this,
+			loop: true
+		});
+
 		this.events.emit("scene-awake");
 	}
 
@@ -48,10 +61,34 @@ export default class Game extends Phaser.Scene
 	{
 		this.player.setGravityY(300);
 	}
+	private spawnPipe(): void
+	{
+		const pipeX = 1200; // Spawn just beyond the right side
+		const pipeY : number = Phaser.Math.Between(100, 400); // Random height for variety
+
+		const randomPipe : number = Phaser.Math.Between(0, 7);
+
+		const pipe = this.pipeGroup.create(pipeX, pipeY, "PipeStyle1", randomPipe);
+		pipe.scaleX = 3;
+		pipe.scaleY = 3;
+		pipe.setVelocityX(-200);
+		pipe.setOrigin(0.5, 0.5);
+
+		pipe.setActive(true);
+		pipe.setVisible(true);
+
+		pipe.body!.onWorldBounds = true;
+		pipe.body!.onCollide = true;
+
+		pipe.body!.checkCollision.left = true;
+		pipe.body!.checkCollision.right = false;
+		pipe.body!.checkCollision.up = false;
+		pipe.body!.checkCollision.down = false;
+	}
 
 	public update() : void
 	{
-		if (this.player.body !== null && this.player.body.velocity.y < 0)
+		if (this.player.body!.velocity.y < 0)
 		{
 			this.player.setAngle(-20);
 		}
@@ -64,5 +101,13 @@ export default class Game extends Phaser.Scene
 		{
 			//this.scene.restart();
 		}
+
+		this.pipeGroup.getChildren().forEach((pipe) =>
+		{
+			if (pipe instanceof Phaser.GameObjects.Image && pipe.x < -50)
+			{
+				pipe.destroy();
+			}
+		});
 	}
 }
